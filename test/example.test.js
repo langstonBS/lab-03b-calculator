96aef99cd49a92f843f90e2422f6af220726a1e1
dl-watson <dlarkinwatson@gmail.com>
2020-09-24 18:51:37 -0700

    added tests

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,97 @@
 // IMPORT MODULES under test here:
+import {
+    addition,
+    subtraction,
+    multiplication,
+    division,
+} from "../mathUtils.js";
+
 // import { example } from '../example.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = false;
+test("addition should return 8 if passed 4 and 4", (expect) => {
+    const expected = 8;
+    const addInput1 = 4;
+    const addInput2 = 4;
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
+    const actual = addition(addInput1, addInput2);
+
+    expect.equal(actual, expected);
+});
+
+test("addition should return 12 if passed 8 and 4", (expect) => {
+    const expected = 12;
+    const addInput1 = 8;
+    const addInput2 = 4;
+
+    const actual = addition(addInput1, addInput2);
+
+    expect.equal(actual, expected);
+});
+
+// test subtractTwoNumbers
+
+test("subtraction should return 2 if passed 6 and 4", (expect) => {
+    const expected = 2;
+    const subtractInput1 = 6;
+    const subtractInput2 = 4;
+
+    const actual = subtraction(subtractInput1, subtractInput2);
+
+    expect.equal(actual, expected);
+});
+
+test("subtraction should return 4 if passed 8 and 4", (expect) => {
+    const expected = 4;
+    const subtractInput1 = 8;
+    const subtractInput2 = 4;
+
+    const actual = subtraction(subtractInput1, subtractInput2);
+
+    expect.equal(actual, expected);
+});
+
+// test multiplyTwoNumbers
+
+test("multiplication should return 8 if passed 2 and 4", (expect) => {
+    const expected = 8;
+    const multiplyInput1 = 2;
+    const multiplyInput2 = 4;
+
+    const actual = multiplication(multiplyInput1, multiplyInput2);
+
+    expect.equal(actual, expected);
+});
+
+test("multiplication should return 16 if passed 2 and 8", (expect) => {
+    const expected = 16;
+    const multiplyInput1 = 2;
+    const multiplyInput2 = 8;
+
+    const actual = multiplication(multiplyInput1, multiplyInput2);
+
+    expect.equal(actual, expected);
+});
+
+// test divideTwoNumbers
+
+test("division should return 4 if passed 8 and 2", (expect) => {
+    const expected = 4;
+    const divideInput1 = 8;
+    const divideInput2 = 2;
+
+    const actual = division(divideInput1, divideInput2);
+
+    expect.equal(actual, expected);
+});
+
+test("division should return 2 if passed 4 and 2", (expect) => {
+    const expected = 2;
+    const divideInput1 = 4;
+    const divideInput2 = 2;
+
+    const actual = division(divideInput1, divideInput2);
+
     expect.equal(actual, expected);
 });
